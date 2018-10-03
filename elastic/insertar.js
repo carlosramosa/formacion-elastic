67ba262bb10eadbb45459a6d950c6eadcad6a14d
carlosramosa <carlosramos.cramos@gmail.com>
2018-10-04 00:40:06 +0200

    refactor(insertar): remove queries.js and add it to insertar.js

--- elastic/insertar.js
+++ elastic/insertar.js
@@ -1,6 +1,13 @@
 'use strict'; 
 
-const { bodyBulkInsert } = require('./queries');
+const pokemon = require('../pokemon.json');
+
+const bodyBulkInsert =
+    pokemon.reduce((acc, item, i) => {
+
+        const obj = { index:  { _index: 'pokemon', _type: 'pokemon', _id: i + 1 } };
+        return acc.concat([obj, item]);
+    }, []);
 
 module.exports = client =>
 
